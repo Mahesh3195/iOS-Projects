3c9136c12430a98516c779a5fc42da0ebc4c3908
MaheshKanike <maheshkanike2@gmail.com>
2020-03-07 15:43:47 +0530

    Initial Commit

---
+++ UrlSessionGetPost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UrlSessionGetPost
+//
+//  Created by K Mahesh on 17/12/1941 Saka.
+//  Copyright © 1941 K Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
